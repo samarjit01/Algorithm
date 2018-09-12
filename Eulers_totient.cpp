3fdc7baf4fbc7bd4936c2616f721b1c8cc7e6601
samarjit01 <samarjitpalnew123@gmail.com>
2018-09-12 20:29:53 +0530

    Add EulerTotient

---
+++ Eulers_totient.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int phi(int n){
+	int result =n;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			while(n%i==0)
+				n/=i;
+			result-=result/i;
+		}
+	}
+
+	if (n>1)
+		result-=result/n;
+	return result;
+}
+
+
+
+int main(){
+
+
+	cout<<phi(11);
+	return 0;
+}
